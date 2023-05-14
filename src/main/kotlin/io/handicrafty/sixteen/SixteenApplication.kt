2a8cf66b8d690008c549d6e505ccb78d1e5aa41a
Dion <ksundong@gmail.com>
2023-05-14 22:01:34 +0900

    first commit

---
+++ src/main/kotlin/io/handicrafty/sixteen/SixteenApplication.kt
@@ -0,0 +1,11 @@
+package io.handicrafty.sixteen
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SixteenApplication
+
+fun main(args: Array<String>) {
+	runApplication<SixteenApplication>(*args)
+}
